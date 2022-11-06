4e1a8d96860373320703681511a898b39c74c7e4
Vitalii Kalinchyk <karlkory@gmail.com>
2022-11-06 12:17:27 +0200

    06.11.22 3

--- src/main/java/com/epam/rd/java/basic/topic08/writer/transformer/UtilityTransformer.java
+++ src/main/java/com/epam/rd/java/basic/topic08/writer/transformer/UtilityTransformer.java
@@ -7,14 +7,24 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.*;
 
 import java.io.*;
+import java.util.Properties;
 
 public class UtilityTransformer {
+
+    private static Properties properties() {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileReader("transformer.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return properties;
+    }
 
     public static void write(Document document, OutputStream output) throws TransformerException {
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.setOutputProperty(OutputKeys.STANDALONE, "yes");
+        transformer.setOutputProperties(properties());
         DOMSource source = new DOMSource(document);
         StreamResult result = new StreamResult(output);
         transformer.transform(source, result);
@@ -23,8 +33,7 @@ public class UtilityTransformer {
     public static void write(OutputStream rawXMLStream, OutputStream output) throws TransformerException {
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.setOutputProperty(OutputKeys.STANDALONE, "yes");
+        transformer.setOutputProperties(properties());
         StreamSource source = new StreamSource(new StringReader(rawXMLStream.toString()));
         StreamResult result = new StreamResult(output);
         transformer.transform(source, result);
